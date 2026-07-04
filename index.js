0384628bcab129098a0ace49d3745fe38506d695
Tithkiry <2024278nguon@aupp.edu.kh>
2026-07-04 11:28:13 +0700

    updated

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 require('dotenv').config();
 
-const TelegramBot =
-    require('node-telegram-bot-api').default;
+const express = require('express');
+const TelegramBot = require('node-telegram-bot-api').default;
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('MovieBot running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
 
 const bot = new TelegramBot(
     process.env.BOT_TOKEN,
